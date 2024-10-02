0edbd87111d8796a496d8812014f0f9295afef01
Luiz Fernando <130947126+OLuizFernando@users.noreply.github.com>
2024-10-01 22:40:28 -0300

    Time: 40 ms (18.24%), Space: 11.6 MB (49.65%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        symbols = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        subtracts = {
+            "I": ["V", "X"],
+            "X": ["L", "C"],
+            "C": ["D", "M"]
+        }
+
+        res = 0
+
+        for i in range(len(s) - 1, -1, -1):
+            if i == len(s) - 1 or s[i] not in ["I", "X", "C"]:
+                res += symbols[s[i]]
+                continue
+
+            if s[i] in ["I", "X", "C"] and s[i + 1] in subtracts[s[i]]:
+                res -= symbols[s[i]]
+            else:
+                res += symbols[s[i]]
+
+        return res
+        
